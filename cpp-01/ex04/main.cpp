6521601e0f8bd850f32629ed6de458f1d1922e13
Artur <vegis.artur@gmail.com>
2024-08-09 17:00:01 +0300

    complete

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -3,18 +3,41 @@
 #include <string>
 
 int	main(int argc, char **argv) {
+	if (argc != 4) {
+		std::cout << "Invalid argument count!!" << std::endl;
+		return (1);
+	}
 	std::string fileName(argv[1]);
+	std::ifstream in(fileName);
+	if (!in.is_open()) {
+		std::cerr << "Unable to open read file" << std::endl;
+		return (1);
+	}
+	std::ofstream out("output.txt", std::ios::out | std::ios::trunc);
+	if (!out.is_open()) {
+		std::cerr << "Unable to open file for writing!!" << std::endl;
+		return (1);
+	}
 	std::string str1(argv[2]);
 	std::string str2(argv[3]);
-
-	std::ifstream file(fileName);
-	if (file.is_open()) {
-		std::string line;
-		while (std::getline(file, line)) {
-
+	std::string newString;
+	std::string line;
+	int			len = str1.length();
+	while (std::getline(in, line)) {
+		while (line.length() > 0) {
+			size_t pos = line.find(str1);
+			if (pos != std::string::npos) {
+				newString = newString + line.substr(0, pos) + str2;
+				line.erase(0, pos+len);
+			} else {
+				newString += line;
+				break ;
+			}
 		}
-		file.close();
-	} else {
-		std::cerr << "Unable to open file" << std::endl;
+		out << newString << std::endl;
+		newString = "";
 	}
+	in.close();
+	out.close();
+	return (0);
 }

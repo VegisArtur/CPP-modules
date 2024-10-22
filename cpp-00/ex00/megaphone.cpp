9c79cc1bd9ee48a7d5c4bbd7c21bbcb2e5a0fc6e
Artur <vegis.artur@gmail.com>
2024-10-22 14:24:03 +0300

    finished

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 
-using namespace std;
-
 int	main(int argc, char **argv)
 {
 	int i = 1;
 	int j = 0;
 
-	(void)argc;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
 	while (argv[i] != NULL)
 	{
 		j = 0;
 		while (argv[i][j] != 0)
 		{
-			argv[i][j] = toupper(argv[i][j]);
+			argv[i][j] = std::toupper(argv[i][j]);
 			j++;
 		}
-		cout << argv[i];
+		std::cout << argv[i];
 		i++;
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return (0);
 }

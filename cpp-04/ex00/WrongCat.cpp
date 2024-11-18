f8d9cdd408032243567a8c2ad57d082176936a67
Artur <vegis.artur@gmail.com>
2024-11-18 19:52:18 +0200

    04 is easy

---
+++ cpp-04/ex00/WrongCat.cpp
@@ -0,0 +1,21 @@
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat()	{
+	std::cout << "Wrong cat constructed" << std::endl;
+	type = "WrongCat";
+}
+
+WrongCat::WrongCat(const WrongCat& other)	{
+	std::cout << "Wrong cat copied" << std::endl;
+	type = other.type;
+}
+
+WrongCat::~WrongCat()	{
+	std::cout << "Wrong cat deconstructed" << std::endl;
+}
+
+WrongCat& WrongCat::operator=(const WrongCat& other)	{
+	if (this != &other)
+		this->type = other.type;
+	return *this;
+}
---
+++ cpp-04/ex00/WrongCat.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "WrongAnimal.hpp"
+
+class WrongCat : public WrongAnimal
+{
+	public:
+		WrongCat();
+		WrongCat(const WrongCat& other);
+		~WrongCat();
+		WrongCat& operator=(const WrongCat& other);
+
+	private:
+
+	protected:
+
+};

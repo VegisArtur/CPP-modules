8d23e65fff17224527ca62f549cf20e7776cdd87
Artur <vegis.artur@gmail.com>
2025-03-26 15:59:50 +0200

    fixed const issue

--- cpp-07/ex00/whatever.hpp
+++ cpp-07/ex00/whatever.hpp
@@ -10,33 +10,33 @@ void swap(V& a, V& b)
 }
 
 template<typename V>
-V min(V a, V b)
+V min(V& a, V& b)
 {
 	if (a < b)
 		return (a);
 	return (b);
 }
 
-// template<typename V>
-// const V min(const V a, const V b)
-// {
-// 	if (a < b)
-// 		return (a);
-// 	return (b);
-// }
+template<typename V>
+const V& min(const V& a, const V& b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
 
 template<typename V>
-V max(V a, V b)
+V& max(V& a, V& b)
 {
 	if (a > b)
 		return (a);
 	return (b);
 }
 
-// template<typename V>
-// const V max(const V a, const V b)
-// {
-// 	if (a > b)
-// 		return (a);
-// 	return (b);
-// }
+template<typename V>
+const V& max(const V& a, const V& b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
